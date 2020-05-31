7cbf4817940e7abee70d5828c58551759d4bcc6b
ryanhanli <43120124+ryanhanli@users.noreply.github.com>
2020-05-30 21:00:56 -0400

    Update Sorting.py

--- Sorting.py
+++ Sorting.py
@@ -58,3 +58,68 @@ def insertionSort(alist):
 alist = [54,26,93,17,77,31,44,55,20]
 insertionSort(alist)
 print(alist)
+
+def shellSort(alist):
+    sublistcount = len(alist)//2
+    while sublistcount > 0:
+
+      for startposition in range(sublistcount):
+        gapInsertionSort(alist,startposition,sublistcount)
+
+      print("After increments of size",sublistcount,
+                                   "The list is",alist)
+
+      sublistcount = sublistcount // 2
+
+def gapInsertionSort(alist,start,gap):
+    for i in range(start+gap,len(alist),gap):
+        currentvalue = alist[i]
+        position = i
+
+        while position>=gap and alist[position-gap]>currentvalue:
+            alist[position]=alist[position-gap]
+            position = position-gap
+
+        alist[position]=currentvalue
+
+alist = [54,26,93,17,77,31,44,55,20]
+shellSort(alist)
+print(alist)
+
+def mergeSort(alist):
+    print("Splitting ",alist)
+    if len(alist)>1:
+        mid = len(alist)//2 #Finding the mid of the array
+        lefthalf = alist[:mid] #Dividing the array elements
+        righthalf = alist[mid:] #into 2 halves
+
+        mergeSort(lefthalf) #Sorting the first half
+        mergeSort(righthalf) #Sorting the second half
+
+        i=0
+        j=0
+        k=0
+        #Copy data to temp arrays lefthalf and righthalf
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] <= righthalf[j]:
+                alist[k]=lefthalf[i]
+                i=i+1
+            else:
+                alist[k]=righthalf[j]
+                j=j+1
+            k=k+1
+        #Checking if any element is leftover
+        while i < len(lefthalf):
+            alist[k]=lefthalf[i]
+            i=i+1
+            k=k+1
+
+        while j < len(righthalf):
+            alist[k]=righthalf[j]
+            j=j+1
+            k=k+1
+    print("Merging ",alist)
+
+alist = [54,26,93,17,77,31,44,55,20]
+mergeSort(alist)
+print(alist)
