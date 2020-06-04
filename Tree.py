07e2e9e276b82319be387edbe024e478ff2cc7ab
ryanhanli <43120124+ryanhanli@users.noreply.github.com>
2020-06-03 23:10:07 -0400

    Added preorder, inorder, and postorder

--- Tree.py
+++ Tree.py
@@ -32,7 +32,52 @@ class BinaryTree:
 
     def getRootVal(self):
         return self.key
+    
+    def preorder(self):
+        print(self.key)
+        if self.leftChild:
+            self.leftChild.preorder()
+        if self.rightChild:
+            self.rightChild.preorder()
+    
+def preorder(tree):
+    if tree:
+        print(tree.getRootVal())
+        preorder(tree.getLeftChild())
+        preorder(tree.getRightChild())
 
+def postorder(tree):
+    if tree != None:
+        postorder(tree.getLeftChild())
+        postorder(tree.getRightChild())
+        print(tree.getRootVal())
+        
+def inorder(tree):
+  if tree != None:
+      inorder(tree.getLeftChild())
+      print(tree.getRootVal())
+      inorder(tree.getRightChild())
+
+def postordereval(tree):
+    opers = {'+':operator.add, '-':operator.sub, '*':operator.mul, '/':operator.truediv}
+    res1 = None
+    res2 = None
+    if tree:
+        res1 = postordereval(tree.getLeftChild())
+        res2 = postordereval(tree.getRightChild())
+        if res1 and res2:
+            return opers[tree.getRootVal()](res1,res2)
+        else:
+            return tree.getRootVal()
+
+def printexp(tree):
+  sVal = ""
+  if tree:
+      sVal = '(' + printexp(tree.getLeftChild())
+      sVal = sVal + str(tree.getRootVal())
+      sVal = sVal + printexp(tree.getRightChild())+')'
+  return sVal
+    
 
 r = BinaryTree('a')
 print(r.getRootVal())
