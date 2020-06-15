1cb820040932a3ec6ffb6dea9c97114417ea2e6e
ryanhanli <43120124+ryanhanli@users.noreply.github.com>
2020-06-14 23:38:15 -0400

    Update league_stt.py

--- league_stt.py
+++ league_stt.py
@@ -9,15 +9,11 @@ typer = Controller()
 
 def speech_to_text():
 	with sr.Microphone() as source:
-		#print('Speak Anything : ')
 		audio = r.listen(source)
 		try:
 			text = r.recognize_google(audio)
-			#print('You said : {}'.format(text))
 		except:
 			text = "Didn't work"
-			#print('Sorry could not recognize your voice')
-	#keyboard.wait("9")
 	if (text == 'quit'):
 		sys.exit()
 	typer.press(Key.enter)
@@ -25,7 +21,6 @@ def speech_to_text():
 	typer.type(text)
 	typer.press(Key.enter)
 	typer.release(Key.enter)
-	#print("You pressed 9")
 
 while True:
 	keyboard.wait("9")
